b3a558050e829d4ffa3aa0e497d963ff1182805b
Huan Wang <huan_wang@g.harvard.edu>
2023-04-03 12:00:34 -0400

    update

--- main.js
+++ main.js
@@ -1,45 +1,19 @@
-// Sample data
-const data = [
-  {
-    date: "2023-01-01",
-    title: "Event 1",
-    image: "https://drive.google.com/drive/u/1/folders/1NV1A2R_VtKodboDvmN5UtJMMAT6iVg0m",
-    description: "This is the description for Event 1."
-  },
-  {
-    date: "2023-02-01",
-    title: "Event 2",
-    image: "https://drive.google.com/drive/u/1/folders/1NV1A2R_VtKodboDvmN5UtJMMAT6iVg0m",
-    description: "This is the description for Event 2."
-  }
-];
+jQuery(document).ready(function($){
+	var $timeline_block = $('.cd-timeline-block');
 
-// Select the timeline container
-const container = d3.select("#timeline");
+	//hide timeline blocks which are outside the viewport
+	$timeline_block.each(function(){
+		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+			$(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		}
+	});
 
-// Create a div for each event
-const events = container.selectAll("div.event")
-  .data(data)
-  .enter()
-  .append("div")
-  .attr("class", "event");
-
-// Add the date
-events.append("div")
-  .attr("class", "date")
-  .text(d => d.date);
-
-// Add the image
-events.append("img")
-  .attr("class", "image")
-  .attr("src", d => d.image);
-
-// Add the title
-events.append("div")
-  .attr("class", "title")
-  .text(d => d.title);
-
-// Add the description
-events.append("div")
-  .attr("class", "description")
-  .text(d => d.description);
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		$timeline_block.each(function(){
+			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+			}
+		});
+	});
+});
